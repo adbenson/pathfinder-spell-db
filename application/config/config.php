5ce819ed08587fee0661706be361adf82bd7f35f
Drew <abenson3@emich.edu>
2011-12-29 18:26:10 -0500

    Eliminated session timeout

--- application/config/config.php
+++ application/config/config.php
@@ -247,7 +247,7 @@ $config['encryption_key'] = '83HKLHkl30011kLlLlLlkhLLKgOIa2wHz';
 |
 */
 $config['sess_cookie_name']		= 'ci_session';
-$config['sess_expiration']		= 7200;
+$config['sess_expiration']		= 0;
 $config['sess_expire_on_close']	= FALSE;
 $config['sess_encrypt_cookie']	= FALSE;
 $config['sess_use_database']	= FALSE;
